88a4b18b8a3fbe890d2b6e92833b6ca4a4271112
Julien Halfkan <j.halfkan@webdigit.be>
2021-01-02 13:50:25 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: "AudioC0RE",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "I2JUdOkcjhU3BYAU4XHfhsHjtvkBd770W0TbncqtDFQ",
+        spaceId: "3hnylw3otkhm",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
