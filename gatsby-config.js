c00aea8d8d4bbb82d05bd80db8fa6ec22506af5d
Halfkan Julien <jookz1@gmail.com>
2021-01-03 15:47:11 +0100

    Add tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-filesystem",
       options: {
