a21ef76f3f9c5f16f344ecbe3860b1f2acb716bd
Halfkan Julien <jookz1@gmail.com>
2021-01-03 14:48:10 +0100

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const AboutPage = () => (
+  <div>
+    <h1>About</h1>
+    <p>AudioC0RE is the leading headphones sharing app in the world!</p>
+  </div>
+)
+
+export default AboutPage
