db9560fb02adbdb220216c850630e2eaedd0837d
Halfkan Julien <jookz1@gmail.com>
2021-01-10 00:04:40 +0100

    Display contentfull Markdown

--- src/templates/location.js
+++ src/templates/location.js
@@ -1,35 +1,43 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
 
 class LocationTemplate extends React.Component {
-  render() {
-    const location = get(this.props, 'data.contentfulLocation')
+    render() {
+        const location = get(this.props, 'data.contentfulLocation')
 
-    return (
-      <>
-        <h1 className="text-green-900 font-bold font-mono uppercase text-5xl py-6">{location.city}</h1>
-        <p>
-            {location.description.description}
-        </p>
-        <div className="uppercase font-mono text-gray-500 hover:text-green-900 hover:line-through">
-            <Link to="/locations/">Back</Link>
-        </div>
-      </>
-    )
-  }
+        return (
+            <>
+                <h1 className="text-green-900 font-bold font-mono uppercase text-5xl py-6">
+                    {location.city}
+                </h1>
+                <div
+                    dangerouslySetInnerHTML={{
+                        __html:
+                            location.childContentfulLocationDescriptionTextNode
+                                .childMarkdownRemark.html
+                    }}
+                ></div>
+                <div className="uppercase font-mono text-gray-500 hover:text-green-900 hover:line-through">
+                    <Link to="/locations/">Back</Link>
+                </div>
+            </>
+        )
+    }
 }
 
 export default LocationTemplate
 
 export const query = graphql`
-  query CityBySlug($slug: String!) {
-    contentfulLocation(slug: { eq: $slug }) {
-      city
-      description {
-          description
-      }
+    query CityBySlug($slug: String!) {
+        contentfulLocation(slug: { eq: $slug }) {
+            city
+            childContentfulLocationDescriptionTextNode {
+                childMarkdownRemark {
+                    html
+                }
+            }
+        }
     }
-  }
 `
